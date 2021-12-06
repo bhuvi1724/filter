1077da8dbb09db84aaadf8106d59958e226a514f
bhuvi1724 <79234626+bhuvi1724@users.noreply.github.com>
2021-12-05 19:29:49 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+nose_x=0;
+nose_y=0;
+function preload(){
+
+}
+function setup(){
+canvas=createCanvas(300, 300);
+canvas.center();
+video=createCapture(VIDEO);
+video.size(300, 300);
+video.hide();
+posenet=ml5.poseNet(video, modelLoaded)
+posenet.on('pose', gotposes)
+}
+function modelLoaded()
+{
+    console.log('poseNet is initalized');
+    
+}
+function draw(){
+image(video, 0, 0, 300, 300);
+}
+function take_snapshot(){
+    Save('myFilterImage.png');
+}
+function gotposes(results)
+{
+    if (results.length > 0)
+    {
+        console.log(results);
+        console.log("nose x=" + results[0].pose.nose.x)
+        console.log("nose y=" + results[0].pose.nose.y)
+        nose_x=results[0].pose.nose.x
+        nose_y=results[0].pose.nose.y
+        fill("purple")
+        circle(nose_x, nose_y, 20)
+    }
+}
+
+
+
